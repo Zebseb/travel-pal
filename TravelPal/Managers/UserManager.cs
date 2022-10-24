af76bef8d0d0e216b3558fddd28748f728f1c48c
Zebseb <113332074+Zebseb@users.noreply.github.com>
2022-10-24 15:57:33 +0200

    Added classes and some props/methods

---
+++ TravelPal/Managers/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Managers
+{
+    public class UserManager
+    {
+        public List<IUser> users = new();
+        public IUser signedInUser;
+
+        public UserManager()
+        {
+            
+        }
+
+        public void AddUser()
+        {
+
+        }
+
+        public void RemoveUser()
+        {
+
+        }
+
+        public void UpdateUsername()
+        {
+            ValidateUsername();
+        }
+
+        private void ValidateUsername()
+        {
+            
+        }
+
+        public void SignInUser()
+        {
+
+        }
+    }
+}
